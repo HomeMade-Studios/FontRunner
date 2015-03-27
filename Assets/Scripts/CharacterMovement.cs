ed398ac0f120a63d665557fedf3a9f988cc75cf1
Romboide82 <ugo98.juve@libero.it>
2015-03-28 00:56:49 +0100

    g

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,7 @@ public class CharacterMovement : MonoBehaviour {
 
 	bool isJumping;
 	public int jumpForce;
+	float jumpStart;
 	GameController gameController;
 
 	// Use this for initialization
@@ -16,10 +17,17 @@ public class CharacterMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-				if(!isJumping){
-					GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-					isJumping = true;
-				}
+			if(!isJumping){
+				GetComponent<Rigidbody2D>().gravityScale = 15;
+				GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+				isJumping = true;
+				jumpStart = Time.time;
+			}
+			else{
+				if(Input.GetTouch(0).phase == TouchPhase.Ended || Time.time - jumpStart >= 0.8f)
+					GetComponent<Rigidbody2D>().gravityScale = 40;
+			}
+
 		}
 		if(Input.GetKeyDown(KeyCode.Space) && !isJumping){
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
